40e4ef9582b2b25be2e1385a6a92922359ff1ca3
ShaileshDevOps1 <shaileshkumarsingh.96@gmail.com>
2026-07-05 02:48:34 +0530

    3rd commit

--- scripts/create-rg.sh
+++ scripts/create-rg.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 set -e
 
-az group create --name rg-devops-lab --location centralindia
+RG_NAME="rg-devops-lab"
+LOCATION="centralindia"
+STORAGE_NAME="stdevops$(date +%s)"
+
+echo "Creating Resource Group..."
+az group create \
+  --name $RG_NAME \
+  --location $LOCATION
+
+echo "Creating Storage Account..."
+az storage account create \
+  --name $STORAGE_NAME \
+  --resource-group $RG_NAME \
+  --location $LOCATION \
+  --sku Standard_LRS
+
+echo "Deployment completed successfully."
